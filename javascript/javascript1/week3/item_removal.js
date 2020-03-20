bc4b9a5894130baadfbecdc4983aadbcea99c52b
gebremedhin85 <gebremedhin1985@gmail.com>
2020-03-20 14:29:58 +0100

    Feedback implemented

--- javascript/javascript1/week3/item_removal.js
+++ javascript/javascript1/week3/item_removal.js
@@ -1,10 +1,10 @@
 //removing an element of an array using splice method
 const names = ['Peter', 'Ahmad', 'Yana', 'kristina', 'Rasmus', 'Samuel', 'katrine', 'Tala'];
 const nameToRemove = 'Ahmad';
- 
-if(names.includes(nameToRemove)) {
-    names.splice(names.indexOf(nameToRemove), 1);
+  
+const foundIndex = names.indexOf(nameToRemove);
+if(foundIndex >=0) {
+    names.splice(foundIndex, 1);
 }
- 
 console.log(names);
 // ['Peter', 'Yana', 'kristina', 'Rasmus', 'Samuel', 'katrine', 'Tala']
