d4a73b7f478e2959bf0005de0df7bfab481a196b
gebremedhin85 <gebremedhin1985@gmail.com>
2020-04-26 18:04:21 +0200

    Implemented feedback

--- javascript/javascript2/week1/averageMedian/average_median.js
+++ javascript/javascript2/week1/averageMedian/average_median.js
@@ -1,22 +1,15 @@
 const housePrices = [3000000,  3500000,  1300000,  40000000,  100000000,  8000000, 2100000];
 
 // to calculate average
-function getAverage(housePrices){
-
-  let sumHousePrice=0;
-
-  for(let i=0; i<housePrices.length; i++){
-
-      sumHousePrice+=housePrices[i];
-  }
-
+ 
+  let sumHousePrice=housePrices.reduce((a,b)=>{
+      return a+b;
+  }, 0);
+ 
+   
   const averageHousePrice=parseInt(sumHousePrice/housePrices.length);
 
-    return averageHousePrice;
-
-};
-
-
+    
 // to get median -
 
 // median is a number on the center of list of ordered numbers or
@@ -28,10 +21,11 @@ function getMedian(housePrices){
     let median=0;
 
     housePrices.sort();
+    const medianPoint=housePrices.length/2;
 
-    if(housePrices.length/2===0){//median  is average of the two ceter numbers
+    if(medianPoint===0){//median  is average of the two ceter numbers
 
-        median=(housePrices[housePrices.length/2 - 1] + housePrices[housePrices.length/2])/2 ;
+        median=(housePrices[medianPoint - 1] + housePrices[medianPoint])/2 ;
 
 
     }else {
@@ -59,7 +53,7 @@ pTag.innerText='House prices: ['+ housePrices +']';
 
 const parTag=document.createElement('p');
 
-parTag.innerText=  'The Average of house prices is : ' + getAverage(housePrices) + ' \n \n And the Median of house prices is : ' + getMedian(housePrices);
+parTag.innerText=  'The Average of house prices is : ' + averageHousePrice + ' \n \n And the Median of house prices is : ' + getMedian(housePrices);
 
 document.body.appendChild(h1Tag);
 
