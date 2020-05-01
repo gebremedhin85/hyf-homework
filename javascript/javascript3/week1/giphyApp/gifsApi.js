463d7507897eaec2b8e264edea5b95226ab001d0
gebremedhin85 <gebremedhin1985@gmail.com>
2020-05-01 16:29:11 +0200

    Task

---
+++ javascript/javascript3/week1/giphyApp/gifsApi.js
@@ -0,0 +1,74 @@
+//Giphy api
+// Create a site where a user can search for any word using gif api
+//https://developers.giphy.com/docs/ 
+//my api id= Q3MkdtrQmN4KgeUL7fqzb5vn1Bjt628g
+
+ 
+const getGifs=()=>{
+    const inputSearching=document.getElementById('inputSearch');
+    
+    let inputWord=inputSearching.value;
+
+    if(inputWord!==''){
+        let url=`https://api.giphy.com/v1/gifs/search?&api_key=Q3MkdtrQmN4KgeUL7fqzb5vn1Bjt628g&q=${inputWord}`;
+
+        fetch(url)
+        .then(res=>res.json())
+        .then(result=>{
+            console.log(result)
+            
+            const div=document.getElementById("gif");
+            const dataArr=result.data;
+            const outputGifs = dataArr.map(dataImage => {
+                return `<li><img src='${dataImage.images.fixed_height_small_still.url}'/></li>`
+            })
+            const output = `
+                <ul>
+                    ${outputGifs.join('')}
+                </ul>
+                `
+            div.innerHTML=output;
+            
+            //If we want to see a given number of gifs
+            const inputNum=document.getElementById('inputNumber');
+            
+            inputNum.addEventListener('input', (event)=>{
+                
+                if(inputNum.value!==''){
+                    
+                    div.innerText='';
+                    
+                    for(let i=0; i<inputNum.value; i++){
+                       
+                        
+                       
+                        const imgTag=document.createElement("img");
+                        
+                        const imageGif=dataArr[i].images.fixed_height_small_still.url;
+                    
+                        imgTag.setAttribute('src', imageGif)
+        
+                        imgTag.setAttribute('width', 250)
+                        imgTag.setAttribute('height', 150)
+                        
+                        imgTag.style.display='block';
+                        
+                        div.appendChild(imgTag);  
+                    }
+                }
+           })
+
+        }) 
+        
+    }else{
+        document.getElementById("gif").innerText='Enter a word to search gifs'
+    }
+
+}
+
+document.getElementById('inputButton').addEventListener('click', getGifs);
+
+//when we need change the searching word
+document.getElementById('inputSearch').addEventListener('keydown', (event)=>{
+    document.getElementById("gif").innerText='';
+});
