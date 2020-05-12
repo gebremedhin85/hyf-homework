301a25746c16e75a705ccf1df91aa3f7ea4ac8db
gebremedhin85 <gebremedhin1985@gmail.com>
2020-05-12 17:33:50 +0200

    Feedback implemented

--- javascript/javascript3/week1/giphyApp/gifsApi.js
+++ javascript/javascript3/week1/giphyApp/gifsApi.js
@@ -13,62 +13,68 @@ const getGifs=()=>{
         let url=`https://api.giphy.com/v1/gifs/search?&api_key=Q3MkdtrQmN4KgeUL7fqzb5vn1Bjt628g&q=${inputWord}`;
 
         fetch(url)
-        .then(res=>res.json())
-        .then(result=>{
-            console.log(result)
-            
-            const div=document.getElementById("gif");
-            const dataArr=result.data;
-            const outputGifs = dataArr.map(dataImage => {
-                return `<li><img src='${dataImage.images.fixed_height_small_still.url}'/></li>`
-            })
-            const output = `
-                <ul>
-                    ${outputGifs.join('')}
-                </ul>
-                `
-            div.innerHTML=output;
-            
-            //If we want to see a given number of gifs
-            const inputNum=document.getElementById('inputNumber');
-            
-            inputNum.addEventListener('input', (event)=>{
-                
-                if(inputNum.value!==''){
-                    
-                    div.innerText='';
-                    
-                    for(let i=0; i<inputNum.value; i++){
-                       
-                        
-                       
-                        const imgTag=document.createElement("img");
-                        
-                        const imageGif=dataArr[i].images.fixed_height_small_still.url;
-                    
-                        imgTag.setAttribute('src', imageGif)
-        
-                        imgTag.setAttribute('width', 250)
-                        imgTag.setAttribute('height', 150)
-                        
-                        imgTag.style.display='block';
-                        
-                        div.appendChild(imgTag);  
-                    }
-                }
-           })
 
-        }) 
+            .then(res=>res.json())
+
+            .then(result=>{
+
+                console.log(result)
+                
+                const div=document.getElementById("gif");
+               
+                const resultArr=result.data;
+               
+                const outputGifs = resultArr.map(dataImage => {
+                   
+                    return `<li><img src='${dataImage.images.fixed_height_small_still.url}'/></li>`
+                })
+
+                const output = `
+                    <ul>
+                        ${outputGifs.join('')}
+                    </ul>
+                    `
+                div.innerHTML=output;
+                
+                //If we want to see a given number of gifs
+                const inputNum=document.getElementById('inputNumber');
+                
+                inputNum.addEventListener('input', (event)=>{
+                    
+                    if(inputNum.value!==''){
+                        
+                        div.innerText='';
+                        
+                        for(let i=0; i<inputNum.value; i++){
+                        
+                            
+                        
+                            const imgTag=document.createElement("img");
+                            
+                            const imageGif=resultArr[i].images.fixed_height_small_still.url;
+                        
+                            imgTag.setAttribute('src', imageGif)
+            
+                                                         
+                            div.appendChild(imgTag);  
+                        }
+                    }
+                })
+
+            }) 
         
-    }else{
+    } else{
+
         document.getElementById("gif").innerText='Enter a word to search gifs'
-    }
+        }
 
 }
+
 
 document.getElementById('inputButton').addEventListener('click', getGifs);
 
 //when we need change the searching word
 document.getElementById('inputSearch').addEventListener('keydown', (event)=>{
+    
     document.getElementById("gif").innerText='';
 });
--- javascript/javascript3/week1/weatherApp/weather_app.js
+++ javascript/javascript3/week1/weatherApp/weather_app.js
@@ -4,6 +4,7 @@
 //  https://api.openweathermap.org/data/2.5/weather?q=copenhagen&appid=a0847d880cfec515e5d8fee8cd7d9fa8
 
 const input=document.getElementById("inputId");
+
 const button=document.getElementById("buttonId");
 
 
@@ -12,43 +13,49 @@ const button=document.getElementById("buttonId");
 const getCity=()=>{
 
    const city=input.value.toLowerCase();
-const url='https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=a0847d880cfec515e5d8fee8cd7d9fa8';
+
+    const url='https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=a0847d880cfec515e5d8fee8cd7d9fa8';
 
     
-    
-    if(city!==""){//checking if we enter city
+    //checking if we enter city
+    if(city!==""){
 
         fetch(url)
-        .then(response => response.json())
-        .then(response => {
-           // console.log(response);
-            document.getElementById("chosenCity").innerHTML=`${response.name} Weather `;
-            
-            //Temperature
-            const temperature=Math.floor(response.main.temp-273);
-            document.getElementById("temperature").innerHTML=` Temperature: ${temperature} °C`; 
-           
-           //wind
-            document.getElementById("windSpeed").innerHTML=`Wind speed: ${Math.floor(response.wind.speed*10/36)} m/s`;
 
-            //Cloud
-            document.getElementById("cloud").innerHTML=`Cloud:  ${Math.floor(response.clouds.all)} % `;
-            
-            //sunrise and sunset
-            
-            const timestamp = response.sys.sunrise;
-            const sunrise = new Date(timestamp * 1000)
-            
+            .then(response => response.json())
 
-            const timestampTwo = response.sys.sunset;
-            const sunset = new Date(timestampTwo * 1000);
-
-            document.getElementById("sunrise").innerHTML=`Sunrise:  ${sunrise} `;
-
-            document.getElementById("sunset").innerHTML=`Sunsets:   ${sunset} `;
-        
+            .then(response => {
                 
-        });
+                // console.log(response);
+                document.getElementById("chosenCity").innerHTML=`${response.name} Weather `;
+                
+                //Temperature
+                const temperature=Math.floor(response.main.temp-273);
+
+                document.getElementById("temperature").innerHTML=` Temperature: ${temperature} °C`; 
+            
+                //wind
+                document.getElementById("windSpeed").innerHTML=`Wind speed: ${Math.floor(response.wind.speed*10/36)} m/s`;
+
+                //Cloud
+                document.getElementById("cloud").innerHTML=`Cloud:  ${Math.floor(response.clouds.all)} % `;
+                
+                //sunrise and sunset
+                const timestamp = response.sys.sunrise;
+
+                const sunrise = new Date(timestamp * 1000)
+                
+
+                const timestampTwo = response.sys.sunset;
+
+                const sunset = new Date(timestampTwo * 1000);
+
+                document.getElementById("sunrise").innerHTML=`Sunrise:  ${sunrise} `;
+
+                document.getElementById("sunset").innerHTML=`Sunsets:   ${sunset} `;
+            
+                    
+            });
        
         //When search for other city again, need to be refreshed
         document.getElementById('inputId').addEventListener('input', (e)=>{
@@ -57,17 +64,13 @@ const url='https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=a084
                 
         })
 
-    }else{ //If there is no input then outputed error message
+
+    //If there is no input then outputed error message
+    }else{ 
+       
         document.getElementById("chosenCity").innerText="Please, enter a city";
         
-        /*document.getElementById("chosenCity").innerHTML="";
-            
-        document.getElementById("temperature").innerHTML=""; 
-        document.getElementById("windSpeed").innerHTML= ""; 
-
-        document.getElementById("cloud").innerHTML= "";
-        document.getElementById("sunrise").innerHTML= "";
-        document.getElementById("sunset").innerHTML= "";*/
+         
         
     }
 }
