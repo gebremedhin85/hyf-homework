6f00b3d4e3496855801534886e33582bd81ed2ab
gebremedhin85 <gebremedhin1985@gmail.com>
2020-05-30 16:47:12 +0200

    Feedback implemented

--- databases/week2/part3Homework.sql
+++ databases/week2/part3Homework.sql
@@ -1,23 +1,34 @@
+use hyf_lesson2;
+
 -- Part 3: More queries
 
 -- Get all the tasks assigned to users whose email ends in @spotify.com 
-SELECT task.id,  task.title AS task_title FROM task
-LEFT JOIN user_task ON user_task.task_id=task.id
-WHERE user_task.user_id=2;
- 
+SELECT task.title, user.email
+FROM user
+    INNER JOIN user_task ON user.id=user_task.user_id
+    INNER JOIN task ON user_task.task_id=task.id
+WHERE user.email LIKE '%@spotify.com';
+
 -- Get all the tasks for 'Donald Duck' with status 'Not started'
-SELECT task.id,  task.title AS task_title, task.status_id AS task_status FROM task
-LEFT JOIN user_task ON user_task.task_id=task.id
-WHERE task.status_id=1
-AND user_task.user_id=11;
+SELECT task.id, task.title AS task_title, user.name
+FROM user
+    INNER JOIN user_task ON user.id = user_task.user_id
+    INNER JOIN task ON user_task.task_id = task.id
+WHERE user.name = 'Donald Duck'
+    AND task.status_id=1;
 
 -- Get all the tasks for 'Maryrose Meadows' that were created in september (hint: month(created)=month_number)
-SELECT task.id,  task.title AS task_title FROM task
-LEFT JOIN user_task ON user_task.task_id=task.id
-WHERE user_task.user_id=6
-AND MONTH(task.created)=09;
+SELECT task.id, task.title AS task_title, month(task.created) AS month_created,
+    user.name AS created_by
+FROM user
+    INNER JOIN user_task ON user.id = user_task.user_id
+    INNER JOIN task ON user_task.task_id = task.id
+WHERE user.name = 'Maryrose Meadows'
+    AND MONTH(task.created)=09;
 
 -- Find how many tasks where created in each month, e.g. how many tasks were created in october, how many tasks were created in november, etc. (hint: use group by)
-SELECT COUNT(month(task.created)) AS task_count FROM task
+SELECT month(task.created) AS month_created,
+    COUNT(month(task.created)) AS task_count
+FROM task
 GROUP BY MONTH(task.created);
 
