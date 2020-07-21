64c7a427e2d3962ff6573d3c02b26b67e50b870d
gebremedhin85 <gebremedhin1985@gmail.com>
2020-07-21 12:49:02 +0200

    Feedback implemented for warmup

--- nodejs/week2/warmup/index.js
+++ nodejs/week2/warmup/index.js
@@ -2,20 +2,18 @@ const express = require("express");
 const app = express();
 
 app.get("/numbers/add", (req, res) => {
-	const firstNum = parseInt(req.query.first);
-	const secondNum = parseInt(req.query.second);
+	const firstNum = parseFloat(req.query.first);
+	const secondNum = parseFloat(req.query.second);
 
-	if (!firstNum) res.send(`Please enter first as a number`);
-	else if (!secondNum) res.send(`Please enter second as a number`);
+	if (!firstNum || !secondNum) res.status(400).send("Bad request. inputes should be numbers");
 	else res.send(`${firstNum + secondNum}`);
 });
 
 app.get("/numbers/multiply/:firstNum/:secondNum", (req, res) => {
-	const firstNum = parseInt(req.params.firstNum);
-	const secondNum = parseInt(req.params.secondNum);
+	const firstNum = parseFloat(req.params.firstNum);
+	const secondNum = parseFloat(req.params.secondNum);
 
-	if (!firstNum) res.send("Please enter a first number");
-	else if (!secondNum) res.send("Please enter a second number");
+	if (!firstNum || !secondNum) res.status(400).send("Bad request. inputes should be numbers");
 	else res.send(`${firstNum * secondNum}`);
 });
 
