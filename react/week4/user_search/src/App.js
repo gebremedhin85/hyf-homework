8ac012a8a05d79bd74b711f5cea4aa98333e1154
gebremedhin85 <gebremedhin1985@gmail.com>
2020-08-24 19:18:29 +0200

    Feedback implemented

--- react/week4/user_search/src/App.js
+++ react/week4/user_search/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import Login from "./Login";
 import { UserContext } from "./UserContext";
-import Users from "./Users";
+import Users from "./Components/Users";
+import Search from "./Components/Search";
 
 export default function App() {
 	const [userId, setUserId] = useState(null || "");
@@ -10,7 +10,7 @@ export default function App() {
 		<div>
 			<h1>Github user searcher</h1>
 			<UserContext.Provider value={{ userId: userId }}>
-				<Login setUserId={setUserId} />
+				<Search setUserId={setUserId} />
 				<Users />
 			</UserContext.Provider>
 		</div>
